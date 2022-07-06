ef2f0eaec9f0829a5ac46be5f2858abd077f71eb
Uunkn0wnN <95006878+Uunkn0wnN@users.noreply.github.com>
2022-07-06 20:36:24 +0700

    Update main.js

--- main.js
+++ main.js
@@ -77,7 +77,7 @@ const connectionOptions = {
   auth: state,
   logger: P({ level: 'silent'}),
   version: [2, 2204, 13],
-  browser: ['AuraBot', 'IOS', '4.1.0']
+  browser: ['https://github.com/Uunkn0wnN', 'IOS', '4.1.0']
 }
 
 global.conn = simple.makeWASocket(connectionOptions)
@@ -96,7 +96,7 @@ async function connectionUpdate(update) {
   if (connection == 'connecting') console.log(chalk.redBright('🕛 Mengaktifkan Bot, Harap tunggu sebentar...'))
   if (connection == 'open') {
       console.log(chalk.green('Connected✅'))
-      await conn.hehe("6281393227036@s.whatsapp.net", global.ftoli)
+      await conn.hehe("628983755600@s.whatsapp.net", global.ftoli)
   }
   if (connection == 'close') console.log(chalk.red('⏹️Koneksi berhenti dan mencoba menghubungkan kembali...'))
   global.timestamp.connect = new Date
