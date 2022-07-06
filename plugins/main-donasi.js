4ad800af5c695b4108056074847be7f943041fb3
Uunkn0wnN <95006878+Uunkn0wnN@users.noreply.github.com>
2022-07-06 21:28:33 +0700

    Update main-donasi.js

--- plugins/main-donasi.js
+++ plugins/main-donasi.js
@@ -2,13 +2,13 @@ const qrku = "http://telegra.ph/file/4728cfbd53c6cbc990413.png"
 
 let handler = async (m, { conn, usedPrefix }) => conn.sendButtonImg(m.chat, qrku, `
 ╭─「 Donasi • Dana 」
-│ • Telkomsel [081393227036]
-│ • Dana  [081393227036]
+│ • three [089512330535]
+│ • three  [6289630985223]
 ╰────
 ╭─「 *NOTE* 」
-│ Scan Qris All payment 
+│ All payment 
 │ Kemudian ingfokan ke 
-│ wa.me/6281393227036
+│ wa.me/628983755600
 ╰────
 `.trim(), wm, 'Menu', usedPrefix + 'menu', m) // Tambah sendiri kalo mau
 handler.help = ['donasi']
