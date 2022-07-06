49ce8e27ce984652ddf1366d7d3e8b25165e3099
Uunkn0wnN <95006878+Uunkn0wnN@users.noreply.github.com>
2022-07-06 21:39:00 +0700

    Update group-intro.js

--- plugins/group-intro.js
+++ plugins/group-intro.js
@@ -2,17 +2,100 @@ let handler = async m => {
 
 let krtu = `Kartu Intro`
 m.reply(`
-0ཻུ۪۪ꦽꦼ̷⸙‹•══════════════♡᭄
-│       *「 Kartu Intro 」*
-│ *Nama     :* 
-│ *Gender   :* 
-│ *Umur      :* 
-│ *Hobby    :* 
-│ *Kelas      :* 
-│ *Asal         :* 
-│ *Agama    :* 
- |  *Status     :* 
-╰═════ꪶ ཻུ۪۪ꦽꦼ̷⸙ ━ ━ ━ ━ ꪶ ཻུ۪۪ꦽꦼ̷⸙
+HI MEMBER BARU SILAHKAN INTRO
+Nama lengkap:
+Nama panggilan:
+Hobi:
+Umur:
+Kelas:
+Tinggi badan:
+Berat badan:
+Agama:
+Golongan darah:
+Status:
+Nama pacar:
+Jumlah mantan:
+Nama mantan:
+Nama bapak:
+Nama ibu:
+Nama kakak:
+Nama adek:
+Nama kakek:
+Nama nenek:
+Nama bibi:
+Nama paman:
+Nama saudara:
+KTP:
+SIM:
+STNK:
+BPKB:
+KK:
+Alamat rumah:
+RT:
+RW:
+KELURAHAN:
+KECAMATAN:
+KABUPATEN:
+KOTA:
+PROVINSI:
+PLANET:
+GALAKSI:
+LANGIT:
+DARATAN:
+LAUTAN:
+KEPULAUAN:
+SAMUDRA:
+UKURAN SEPATU:
+UKURAN BAJU:
+UKURAN CELANA:
+LEBAR PINGGANG:
+PANJANG TANGAN:
+PANJANG KAKI:
+MAKANAN FAVORIT:
+MINUMAN FAVORIT:
+FILM FAVORIT:
+SINETRON FAVORIT:
+GAME FAVORIT:
+CHANNEL YOUTUBE:
+INSTAGRAM:
+TWITTER:
+SNAPCHAT:
+FACEBOOK:
+TIK TOK:
+LIKEE:
+MUSIC FAVORIT:
+SIFAT:
+SIKAP:
+ZODIAK:
+TANGGAL LAHIR:
+MERK HP:
+MERK MOTOR:
+MERK MOBIL:
+TINGKAT RUMAH:
+ALAMAT SEKOLAH:
+EMAIL:
+PASSWORD EMAIL:
+ELEMEN ZODIAK:
+JUMLAH KEKAYAAN:
+BERAK SEHARI BERAPA KALI:
+BEKERJA DI:
+OVO:
+GOPAY:
+TOKEN:
+DISCORD:
+ID:
+JAM:
+MENIT:
+DETIK:
+MILIDETIK:
+TANGGAL:
+BULAN:
+TAHUN:
+ABSEN:
+PASSWORD:
+BATERAI:
+PLANET:
+GALAKSI:
 `.trim()) // Tambah sendiri kalo mau
 }
 handler.command = /^(intro)$/i
