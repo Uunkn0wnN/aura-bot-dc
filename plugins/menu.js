1193023c95182c2b903441301d37f684c4dfdbfd
Uunkn0wnN <95006878+Uunkn0wnN@users.noreply.github.com>
2022-07-07 11:32:42 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -11,9 +11,9 @@ const defaultMenu = {
 ├ Database *%totalreg*
 └────
 %readmore`.trim(),
-  header: '┌─〔 %category 〕',
-  body: '├ %cmd %islimit %isPremium',
-  footer: '└────\n',
+  header: '╭───「 %category 」',
+  body: '├ • %cmd %islimit %isPremium',
+  footer: '╰───「 *AuraBot* 」\n',
   after: `
 `,
 }
